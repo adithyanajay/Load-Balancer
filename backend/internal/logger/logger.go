25e75fc9abd22dbc38bc94976d21496277301878
adithyanajay <adithyanajay@gmail.com>
2026-02-09 08:25:01 +0530

    logger

--- backend/internal/logger/logger.go
+++ backend/internal/logger/logger.go
@@ -19,7 +19,6 @@ func ThresholdState(under []string, over []string) {
 	)
 }
 
-
 func Info(msg string) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -27,28 +26,27 @@ func Info(msg string) {
 	log.Printf("[INFO] %s | %s", time.Now().Format(time.RFC3339), msg)
 }
 
-func StateChange(vmID string, from string, to string) {
+func StateChange(instanceID string, from string, to string) {
 	mu.Lock()
 	defer mu.Unlock()
 
 	log.Printf(
-		"[STATE] %s | VM=%s | %s -> %s",
+		"[STATE] %s | Instance=%s | %s -> %s",
 		time.Now().Format(time.RFC3339),
-		vmID,
+		instanceID,
 		from,
 		to,
 	)
 }
 
-func MetricsUpdate(vmID string, load float64) {
+func MetricsUpdate(instanceID string, load float64) {
 	mu.Lock()
 	defer mu.Unlock()
 
 	log.Printf(
-		"[METRICS] %s | VM=%s | Load=%.2f%%",
+		"[METRICS] %s | Instance=%s | Load=%.2f%%",
 		time.Now().Format(time.RFC3339),
-		vmID,
+		instanceID,
 		load,
 	)
 }
-
